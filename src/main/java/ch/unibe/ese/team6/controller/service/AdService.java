543ea88c4a06305346b7c47703981b434c8ee378
Valerie Hafkta <valerie.haftka@students.unibe.ch>
2016-10-26 16:18:53 +0200

    modified the adservice again

--- src/main/java/ch/unibe/ese/team6/controller/service/AdService.java
+++ src/main/java/ch/unibe/ese/team6/controller/service/AdService.java
@@ -251,12 +251,12 @@ public class AdService {
 	 */
 	private void sendEmail(User temp, User sender, String subject, String txt) {
 		Properties properties = System.getProperties();
-		properties.setProperty("mail.smtp.host", "localhost");
+		properties.setProperty("mail.smtp.host", "smtp.unibe.ch");
 		javax.mail.Session session = javax.mail.Session.getInstance(properties);
 
 		try {
 			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(sender.getEmail()));
+			message.setFrom(new InternetAddress("valerie.haftka@students.unibe.ch")); //sender.getEmail()
 			message.addRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(temp.getEmail(), false));
 			message.setSubject(subject);
 			message.setText(txt);
