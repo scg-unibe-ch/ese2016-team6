431266a40f930a70ae63412101add6777f92adbc
patrick indermuehle <patrick.indermuehle@students.unibe.ch>
2016-12-14 08:12:21 +0100

    fixed the enquiries display in the header so it does not display "undefined"

--- src/main/webapp/js/newEnquiries.js
+++ src/main/webapp/js/newEnquiries.js
@@ -3,6 +3,6 @@
 function newEnquiries(place) {
 	$.get("/profile/newEnquiries", function(data){
 		var enquiry;
-		$("#enquiryLink").html(data + " " + enquiry);
+		$("#enquiryLink").html("Enquiries ("+ data + ")" );
 	});
 }
